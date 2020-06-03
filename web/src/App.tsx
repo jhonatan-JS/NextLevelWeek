7e5b8988759725c90ab62fe0191c87c5acffeafe
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-06-03 13:22:37 -0300

    conceitos do React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+// JSX: Sintaxe de XLM dentro do javaScript
+import Header from './Header';
+
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+      setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header title={ `Contador: ${counter}` }/>
+
+        <h1> {counter} </h1>
+        <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
 export default App;
+
+
+//div#app>ul>li*5
