4e7d290510fbb486d582c60f11b4af3290c4c0b0
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-06-03 20:23:44 -0300

    Install axios e criar api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-// JSX: Sintaxe de XLM dentro do javaScript
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-      setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={ `Contador: ${counter}` }/>
-
-        <h1> {counter} </h1>
-        <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+  <Routes />
   );
 }
 
 export default App;
-
-
-//div#app>ul>li*5
